ed5348dc214aa487a37b550109074f2a0fd852a2
cMexypaH <33319263+cMexypaH@users.noreply.github.com>
2020-06-06 19:19:24 +0300

    Reformat the code

--- main.py
+++
@@ -1,9 +0,0 @@
-import commands
-
-while True:
-    command = input("(O)pen or (C)lose doors: ").upper()
-    t = input("Time: ")
-    if command == "O":
-        commands.opendoor(t)
-    elif command == "C":
-        commands.closedoor(t)
--- rfcomm.py
+++ rfcomm.py
@@ -5,34 +5,49 @@ import RPi.GPIO as GPIO
 from bluetooth import *
 import commands
 
-pin11 = 11
-pin13 = 13
-pin15 = 15
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(pin11, GPIO.OUT)
-GPIO.setup(pin13, GPIO.OUT)
-GPIO.setup(pin15, GPIO.OUT)
+connected = False
 
 server_sock = BluetoothSocket(RFCOMM)
 server_sock.bind(("", PORT_ANY))
+
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "00001101-0000-1000-8000-00805f9b34fb"
 
 advertise_service(server_sock, "GragePiServer",
                   service_id=uuid,
                   service_classes=[uuid, SERIAL_PORT_CLASS],
                   profiles=[SERIAL_PORT_PROFILE],
-                  #                   protocols = [ OBEX_UUID ]
                   )
-print("Waiting for connection on RFCOMM channel %d" % port)
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from ", client_info)
+
+def is_connected():
+    global connected
+    try:
+        client_sock.getpeername()
+        connected = True
+    except:
+        connected = False
+
+
+def listen_for_connection():
+    global server_sock, port, client_sock, client_info
+    print("Waiting for connection on RFCOMM channel %d" % port)
+    client_sock, client_info = server_sock.accept()
+    print("Accepted connection from ", client_info)
+
+
+def disconnect(c_sock, s_sock):
+    print("disconnected")
+    c_sock.close()
+    s_sock.close()
+    print("all done")
+
+
+listen_for_connection()
+
 while True:
 
     try:
@@ -40,7 +55,8 @@ while True:
         data = data.decode('ascii')
         data = data.lower()
         if len(data) == 0:
-            break
+            #disconnect(client_sock, server_sock)
+            listen_for_connection()
         print("received [%s]" % data)
 
         commands.command(data)
@@ -52,11 +68,5 @@ while True:
         pass
 
     except KeyboardInterrupt:
-
-        print("disconnected")
-
-        client_sock.close()
-        server_sock.close()
-        print("all done")
-
+        disconnect(client_sock, server_sock)
         break
